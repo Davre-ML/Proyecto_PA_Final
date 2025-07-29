87916d455fbf65f9466c39051a7cc6958b865824
Davre-ML <davidmlopez1306@gmail.com>
2025-07-28 22:10:19 -0600

    Add files via upload

---
+++ ProyectoFinal.py
@@ -0,0 +1,24 @@
+"""Welcome to Reflex! This file outlines the steps to create a basic app."""
+import reflex as rx
+import reflex.components as rxc
+from reflex import State
+
+from rxconfig import config
+
+##pages
+from ProyectoFinal.pages.registro import registro
+from ProyectoFinal.pages.inicio_sesion import inicio_sesion
+
+class State(rx.State):
+    """The app state."""
+
+    ...
+
+
+    
+
+
+app = rx.App()
+app.add_page(inicio_sesion, route="/")
+app.add_page(registro, route="/registro")
+
